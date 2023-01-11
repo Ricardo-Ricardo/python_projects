1e6f0a1a4a17aa3b26670b133a939ae27b1052bf
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-11 16:14:45 -0600

    Added grid format

---
+++ TicTacToe.py
@@ -0,0 +1,17 @@
+playerchoice1 = input("Enter Row and Column(A 1): ")
+
+def printboard(playerchoice1):
+    print("     *     *     ")
+    print("     *     *     ")
+    print("     *     *     ")
+    print("*" * 17)
+    #print(playerchoice1)
+    print("     *     *     ")
+    print("     *     *     ")
+    print("     *     *     ")
+    print("*" * 17)
+    print("     *     *     ")
+    print("     *     *     ")
+    print("     *     *     ")
+
+printboard(playerchoice1)
