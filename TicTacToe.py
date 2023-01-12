f07207067332ecf882a0c3caec1ec0b4fd9c3680
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-11 22:46:38 -0600

    Created object for game board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,17 +1,57 @@
 playerchoice1 = input("Enter Row and Column(A 1): ")
 
-class printboard:
-    print("        |  O  | X X ")
-    print("        | O O |  X  ")
-    print("        |  O  | X X ")
-    print(" " + " -" * 10)
-    #print(playerchoice1)
-    print("        |  O  |     ")
-    print("        | O O |     ")
-    print("        |  O  |     ")
-    print(" " + " -" * 10)
-    print("    X X |  O  |     ")
-    print("     X  | O O |     ")
-    print("    X X |  O  |     ")
+
+def gameboard():
+    card = []
+    rows = [1, 2, 3]
+    columns = ["A", "B", "C"]
+    for row in rows:
+        for col in columns:
+            card.append([row, col, 1]) # 1 means empty # 2 means X # 3 means O
+    return card
+
+#### Example Board
+#def printboard():
+#    print("     |  O  | X X ")
+#    print("     | O O |  X  ")
+#    print("     |  O  | X X ")
+#    horizontal()
+#    #print(playerchoice1)
+#    print("     |     |     ")
+#    print("     |     |     ")
+#    print("     |     |     ")
+#    horizontal()
+#    print(" X X |  O  |     ")
+#    print("  X  | O O |     ")
+#    print(" X X |  O  |     ")
+
+def printX(line):
+    if line == 1:
+        print(" X X ")
+    else:
+        print("  X  ")
+def printO(line):
+    if line == 1:
+        print("  O  ")
+    else:
+        print(" O O ")
+def horizontal():
+    horizontal = print(" -" * 10)
+    return horizontal
+
+card = gameboard()
+print(card)
+class printboard():
+    def __init__(self):
+        self.templine = ""
+        for i in range(3):
+            if card[i][2] == 1:
+                self.templine += ("     ")
+            elif card[i][2] == 2:
+                printX()
+            else:
+                printO()
+        print(self.templine)
+        horizontal()
 
 printboard()
