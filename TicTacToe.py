90764f8abdf039a5e8f0fdd5de93a4efcd83b98f
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-12 09:50:07 -0600

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -33,20 +33,22 @@ class printboard():
     def __init__(self):
         self.templine = ""
         for i in range(1,10):
+            self.templine += "  "
             if i == 2 or i == 5 or i ==8:
                 self.line = 2
             else:
                 self.line = 1
             for j in range(3):
                 if card[j][2] == 1: # 1 means empty
-                    #self.templine += ("     ")
-                    self.printX(1)
+                    self.templine += ("     ")
                 elif card[j][2] == 2: # 2 means X
-                    self.printX()
+                    self.printX(self.line)
                 else:
-                    self.printO() # 3 means O
+                    self.printO(self.line) # 3 means O
+                if j == 0 or j == 1:
+                    self.templine += ("|")
             self.templine += "\n"
-            if i == 3 or i == 6:
+            if i == 3 or i == 6: # These are the horizontal lines
                 self.templine += (" -" * 10 + "\n")
         print(self.templine)
         #horizontal()
@@ -62,4 +64,8 @@ class printboard():
             self.templine += (" O O ")
     #def horizontal(self):
         #self.horizontal += (" -" * 10)
+
+print(card[1][2])
+card[1][2] = 2
+print(card[1][2])
 printboard()
