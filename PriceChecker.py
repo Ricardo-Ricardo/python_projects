fe58c4cdc729b1e9dc9d6878660f9cfe0e6eba66
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-16 23:21:53 -0600

    different format

--- PriceChecker.py
+++ PriceChecker.py
@@ -6,14 +6,14 @@ URL = 'https://www.target.com/p/lego-star-wars-501st-clone-troopers-battle-pack-
 #URL = 'https://www.lego.com/en-us/product/501st-clone-troopers-battle-pack-75345'
 #headers = {"User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15'}
 
-#page = requests.get(URL, headers=headers)
+page = requests.get(URL, headers=headers)
 
-#soup = BeautifulSoup(page.content, 'html.parser')
+soup = BeautifulSoup(page.content, 'html.parser')
 
-#title = soup.find(id="productTitle")
-#title = soup.find(id="mw-content-text").get_text()
-#title = soup.find("span", {"class": "product-price", "data-test": True})['data-test']
-#print(title.strip())
+title = soup.find(id="productTitle")
+title = soup.find(id="mw-content-text").get_text()
+title = soup.find("span", {"class": "product-price", "data-test": True})['data-test']
+print(title.strip())
 with requests.Session() as s:
     s.headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.6.1 Safari/605.1.15'
     res = s.get(URL)
