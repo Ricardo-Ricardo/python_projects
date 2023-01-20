e5bc7b1672e01504f1d3eeefbd7299512b0351ad
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-20 14:30:01 -0600

    create a basic window

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+import tkinter
+
+window = tkinter.Tk() # main window
+window.minsize(400,400) # size of window in pixels
+window.title("Title")    # title of window
+window.wm_iconbitmap("icon_name.ico") # set icon
+window.mainloop() 
