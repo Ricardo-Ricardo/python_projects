a2d29888b6c86a0a460af467cd2198e241c3cb02
Ricardo-Ricardo <92102618+Ricardo-Ricardo@users.noreply.github.com>
2023-01-20 14:45:35 -0600

    Added label and button

--- Calculator.py
+++ Calculator.py
@@ -4,4 +4,25 @@ window = tkinter.Tk() # main window
 window.minsize(400,400) # size of window in pixels
 window.title("Title")    # title of window
 window.wm_iconbitmap("icon_name.ico") # set icon
+
+
+def center(window):
+    window.update_idletasks() # redraw widgets
+
+    width = window.winfo_width() 
+    height = window.winfo_height()
+
+    x = (window.winfo_screenwidth() // 2) - (width // 2) # calculate geometry
+    y = (window.winfo_screenheight() // 2) - (height // 2)
+
+    window.geometry('{}x{}+{}+{}'.format(width, height, x, y)) # set geometry
+
+center(window)
+
+label = tkinter.Label(window, text = "Click the Button")
+label.place(x=10, y=10)
+
+button = tkinter.Button(window, text="Button")
+button.place(x = 10, y = 40)
+
 window.mainloop() 
